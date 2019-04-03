96a3270014d8a28d2ec0f9c73e0a0071c9aab372
AndreasMaeurer <amaeurer@miners.utep.edu>
2019-04-02 23:17:52 -0600

    Added Arguments and Structure for Rest of Code

--- photoCopy.c
+++ photoCopy.c
@@ -1,4 +1,5 @@
-/***********************************************************************  
+/***********************************************************************   
+ * 
  * @Author
                      _                      __  __                                
      /\             | |                    |  \/  |                               
@@ -8,16 +9,17 @@
  /_/    \_\_| |_|\__,_|_|  \___|\__,_|___/ |_|  |_|\__,_|\___|\__,_|_|  \___|_| 
      
 
-Scope: Make a photocopy in one click on my Ubuntu Laptop with my Canon Scanner and my HP Printer
+Scope: Make a photocopy in one click on my Ubuntu Installation with my Canon Scanner and my HP Printer
 
-* Start
-	* This program uses my Scanner (my specific Cannon) to scan whatever is on the glass.
-	* It then saves this file in a temporary directory (In my case: ) called raw.tiff
-	* It then takes that file and prints it "as is" to my HP printer
-	* It then creates a cropped file called output.tiff
-	* It then deletes the raw.tiff file and the output.tiff file
-* End 
-* 
+PseudoCode:
+	* Start
+		* This program uses my Scanner (my specific Cannon) to scan whatever is on the glass.
+		* It then saves this file in a temporary directory (In my case: ) called raw.tiff
+		* It then creates a cropped file called output.tiff
+		* It then takes that file and prints it "as is" to my HP printer	
+		* It then deletes the raw.tiff file and the output.tiff file
+	* End 
+	* 
 
 Notes:  
 * On how to scan and print from the command line (to emulate a Photocopier with one click)
@@ -36,24 +38,109 @@ Notes:
 	
 		works:
 		lp -d HP-LaserJet-1020 raw.tiff
-
+Tested On:
+	Ubuntu 18.04.2 LTS
+	Ubuntu...
+	
 Future:
-* Make a python script?
-* shell script?
-* find a nicer icon?
-* 
+	* Make a python script?
+	* shell script?
+	* find a nicer icon / find a way to compile with a custom icon 	
+	* write code to measure how long it took to Scan the Document.   (Maybe measure how long it takes to print??)
+	* add a debug mode to the code?  (boolean named debug... If TRUE then all the debug statements are executed.  Else not...
+	* how can you get the info from here: http://localhost:631/printers/   Through the Command Line?
+	* 
 ***********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <string.h>
 
-int main() {
+int version() {
+	system("echo photoCopy Version: 0.0.01beta");
+}
+
+int help() {
+	system("cat /home/ernesto/from/Andreas/photoCopy/photoCopy.help");
+}
+
+int show() {
+	system("echo FIXME If this part was completed yet, then you would be getting the list of all printers and scanners here");
+}
+
+int showPrinters() {
+	system("echo FIXME If this part was completed yet, then you would be getting the list of all PRINTERS here");
+}
+
+int showScanners() {
+	system("echo FIXME If this part was completed yet, then you would be getting the list of all SCANNERS here");
+}
+
+int normalExecution() {
+	// The Scanner should be a variable...
+	// The Printer should be a variable...
+	// Maybe DPI should be a variable ?
+	// Perhaps all of these should be global variables...?
+	// Need to figure out what the best DPI to use is...  600 could be considered too slow.  But it's Resolution is nice.
 	
-	system("scanimage -d 'pixma:04A9176D_2F2912' --format tiff > /tmp/raw.tiff");
-	system("convert raw.tiff -crop 638x816+0+0 output.tiff");	//(widthxheight+left+top / wxh+l+t format)
-	system("lp -o page-bottom=N -o page-left=N -o page-right=N -o page-top=N -d HP-LaserJet-1020 /tmp/output.tiff");
+	system("scanimage -d 'pixma:04A9176D_0F27CE' --resolution 600dpi --format tiff > /tmp/raw.tiff"); //Scans 5104 X 7016  Needs to be cropped to: 5104 X 6575
+	system("convert /tmp/raw.tiff -crop 5104x6575+0+0 /tmp/output.tiff");	//(widthxheight+left+top / wxh+l+t format)  //638x820
+	system("lp -o page-bottom=N -o page-left=N -o page-right=N -o page-top=N -d HP_LaserJet_P1505 /tmp/output.tiff");
 	system("rm /tmp/raw.tiff");
 	system("rm /tmp/output.tiff");
+}
+
+// Taking argument as command line 
+int main(int argc, char *argv[]) {
+
+	//This whole next section, that handles the Arguments, seems a bit clumsy...
+	
+	//List the strings that we are going to compare later:
+	char s00[100] = "-v";
+	char s01[100] = "-V";
+	char s02[100] = "--version";
+	char s03[100] = "-s";
+	char s04[100] = "--show";
+	char s05[100] = "-sp";
+	char s06[100] = "--show-printers";
+	char s07[100] = "-ss";
+	char s08[100] = "--show-scanners";	
+	
+	// IF the Argument Vector is 1 THEN just execute the normalExecution ... of the code...
+	if (argc == 1) {
+		normalExecution();
+		//printf("The normal Execution of the code has been reached.\n");  // for debugging
+		return 0; 
+	}	
+	// IF the argument is -v or -V or --version THEN print the Version to the command prompt.
+	if ( (strcmp(argv[1],s00) == 0) || (strcmp(argv[1],s01) == 0) || (strcmp(argv[1],s02) == 0) ) {	//-v -V --version
+		printf("version has been reached.\n");  // for debugging
+		version();
+		return 0;
+	}	
+	//show
+	if ( (strcmp(argv[1],s03) == 0) || (strcmp(argv[1],s04) == 0) ) {	//-s --show
+		printf("show has been reached.\n");  // for debugging
+		show();
+		return 0;
+	}	
+	//show Printer
+	if ( (strcmp(argv[1],s05) == 0) || (strcmp(argv[1],s06) == 0) ) {	//-sp --show-printers
+		printf("show PRINTERS has been reached.\n");  // for debugging
+		showPrinters();
+		return 0;
+	}	
+	//show Scanner
+	if ( (strcmp(argv[1],s07) == 0) || (strcmp(argv[1],s08) == 0) ) {	//-ss --show-scanners
+		printf("show SCANNERS has been reached.\n");  // for debugging
+		showScanners();
+		return 0;
+	}
+	else {
+		help();
+		return 0;
+	}
 }
 
 //When I grow up I hope to be the kind of person who expects too much from sunsets
